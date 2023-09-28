ed5eed89ad5ed13da76d68685ccc48538dc546e3
TrungHoang <ha2kv3@gmail.com>
2023-09-28 17:38:55 +0700

    config database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try{
-        await mongoose.connect('mongodb://127.0.0.1/led_dev')
+        await mongoose.connect('mongodb+srv://ha2kv3:8VRLXneq2Zhh3QWf@ledserver.g8kfp7j.mongodb.net/?retryWrites=true&w=majority')
         console.log("Connect Succesfully");
     } catch(err) {
         console.error(err)
